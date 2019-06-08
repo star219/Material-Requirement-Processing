8576ca69756af924dc751aeaee33aa81d23581e0
bear49193 <rbeer199525@gmail.comgit>
2019-06-08 17:08:03 +0530

    Update Tables.sql

--- Tables.sql
+++ Tables.sql
@@ -1,22 +1,16 @@
+
 CREATE TABLE bom (itno NUMBER(4),name varchar(15), rate NUMBER(7)); 
+INSERT INTO bom (itno, name, rate) VALUES (1, 'Yarn', 46);
+INSERT INTO bom (itno, name, rate) VALUES (2, 'Fabric', 80);
+INSERT INTO bom (itno, name, rate) VALUES (3, 'Dye', 30);
+INSERT INTO bom (itno, name, rate) VALUES (4,'Decorative',12);
 
-SELECT * FROM bom;
-SELECT rate FROM bom WHERE name = 'Yarn';
+CREATE TABLE mrp (productno NUMBER(5),pname VARCHAR(20),YARNQTY NUMBER(7),FABRICQTY NUMBER(7),DYEQTY NUMBER(7),DECORATIVESQTY NUMBER(7));
+INSERT INTO mrp (productno,pname,YARNQTY,FABRICQTY,DYEQTY,DECORATIVESQTY) VALUES (7011,'Shirt',5,5,1,12);
+INSERT INTO mrp (productno,pname,YARNQTY,FABRICQTY,DYEQTY,DECORATIVESQTY) VALUES (7012,'Pant',7,6,2,20);
+INSERT INTO mrp (productno,pname,YARNQTY,FABRICQTY,DYEQTY,DECORATIVESQTY) VALUES (7013,'Saree',4,7,3,18);
 
-INSERT INTO bom (itno, name, rate) VALUES (4,'Decoratives',12);
-
-CREATE TABLE mrp (productno NUMBER(5),pname VARCHAR(20),YARNQTY NUMBER(7),FABRICQTY NUMBER(7),DYEQTY NUMBER(7),DECORATIVES NUMBER(7));
-
-INSERT INTO mrp (productno,pname,YARNQTY,FABRICQTY,DYEQTY,DECORQTY) VALUES (7013,'Saree',4,7,3,18);
-
-SELECT * FROM mrp;
-
-CREATE TABLE demand (DEDATE DATE NOT NULL,YARNDMD NUMBER(10),FABRICDMND NUMBER(10),DYEDMD NUMBER(10),DECORATIVESDMD NUMBER(10),PRODUCTNO NUMBER(7) );
-Alter table demand ADD PRODUCTNO NUMBER(7)  NOT NULL;
-
-SELECT * FROM demand;
-DROP TABLE PRODEMAND;
-
+CREATE TABLE demand (DEDATE DATE NOT NULL,YARNDMD NUMBER(10),FABRICDMND NUMBER(10),DYEDMD NUMBER(10),DECORATIVESDMD NUMBER(10),PRODUCTNO NUMBER(7));
 CREATE TABLE prodemand(DEDATE DATE ,pname VARCHAR(20),productno NUMBER(5),produtreq NUMBER(7));
 
 
@@ -26,12 +20,11 @@ CREATE TABLE prodemand(DEDATE DATE ,pname VARCHAR(20),productno NUMBER(5),produt
 
 
 
-
-
+// don't run below queries
+                                                                                           
 INSERT INTO prodemand (DEDATE, pname, productno, produtreq) VALUES (TO_DATE('01/01/19','DD/MM/YYYY'),'Shirts',7011,40);
 INSERT INTO prodemand (DEDATE, pname, productno, produtreq) VALUES (TO_DATE('01/01/2019','DD/MM/YYYY'),'Pants',7012,23);
 INSERT INTO prodemand (DEDATE, pname, productno, produtreq) VALUES (TO_DATE('01/01/2019','DD/MM/YYYY'),'Sarees',7013,34);
-INSERT INTO prodemand (DEDATE, pname, productno, produtreq) VALUES (TO_DATE('01/01/2019','DD/MM/YYYY'),'Trousers',7014,56);
 
 SELECT produtreq FROM prodemand WHERE dedate = '01/01/2019' AND pname = 'Decoratives';
 SELECT * FROM prodemand;
@@ -40,7 +33,4 @@ INSERT INTO demand (DEDATE, YARNDMD, FABRICDMND, DYEDMD, DECORATIVESDMD,PRODUCTN
 VALUES (TO_DATE('01/01/2019','DD/MM/YYYY'),
 ((SELECT produtreq FROM prodemand WHERE dedate = '01/01/2019' AND PRODUCTNO  = 7011)*(SELECT YARNQTY FROM MRP WHERE PRODUCTNO = 7011)),
 ((SELECT produtreq FROM prodemand WHERE dedate = '01/01/2019' AND PRODUCTNO  = 7011)*(SELECT FABRICQTY FROM MRP WHERE PRODUCTNO = 7011)),
-((SELECT produtreq FROM prodemand WHERE dedate = '01/01/2019' AND PRODUCTNO  = 7011)*(SELECT DYEQTY FROM MRP WHERE PRODUCTNO = 7011)),
-((SELECT produtreq FROM prodemand WHERE dedate = '01/01/2019' AND PRODUCTNO  = 7011)*(SELECT DECORQTY FROM MRP WHERE PRODUCTNO = 7011))
-,7011);
-
+((SELECT produtreq FROM prodemand WHERE dedate = '01/01/2019' AND PRODUCTNO  = 7011)*(SELECT DYEQTY FROM MRP WHERE PRODUCTNO = 7011));
